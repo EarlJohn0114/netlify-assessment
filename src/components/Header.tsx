d2712726dcdcae50954f932e69749795d66e0464
EarlJohn0114 <earljohn.mandin09@gmail.com>
2025-11-02 20:29:24 +0800

    fix error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,6 @@ const Header = () => {
     element?.scrollIntoView({ behavior: "smooth" });
   };
 
-  //push lang
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
